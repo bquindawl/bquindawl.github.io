a81549f24abc83ec9a9690f1b32348c8e1bf809d
Clay <151966173+C1aYalC@users.noreply.github.com>
2025-10-27 12:21:06 -0400

    updates for offline usage

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,37 @@
 const CACHE_NAME = 'steamcalc-v1';
-const URLS_TO_CACHE = [
+const urlsToCache = [
   './',
   './index.html',
-  './style.css',
+  './styles.css',
   './script.js',
   './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  console.log('[Service Worker] Installed');
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+  console.log('[Service Worker] Activated');
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      // Serve cached file or fetch new one
+      return response || fetch(event.request);
+    })
   );
 });
